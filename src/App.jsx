bc98ee9b70d84e589a3232949d34089690285586
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-12-15 11:50:03 +0000

    final challenge done

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,36 @@ import "./App.scss";
 import Tile from "./Compenents/Tile/Tile";
 import pokemonArray from "./Data/pokemon";
 
-const App = () => {
-  const pokemonTileJsx = pokemonArray.map((pokemon) => (
-    <Tile
-      key={pokemon.id}
-      name={pokemon.name}
-      id={pokemon.id}
-      types={pokemon.types}
-      image={pokemon.sprite}
-    />
-  ));
+//const App = () => {
+//const pokemonTileJsx = pokemonArray.map((pokemon) => (
+//<Tile
+//key={pokemon.id}
+//name={pokemon.name}
+//id={pokemon.id}
+//types={pokemon.types}
+//image={pokemon.sprite}
+///>
+//));
 
+const App = () => {
+  const pokemonTileJsxGround = pokemonArray
+    .filter(
+      (pokemonGround) =>
+        pokemonGround.types == "water" && pokemonGround.id > 130
+    )
+    .map((pokemon) => (
+      <Tile
+        key={pokemon.id}
+        name={pokemon.name}
+        id={pokemon.id}
+        types={pokemon.types}
+        image={pokemon.sprite}
+      />
+    ));
   return (
     <div>
       <h1> Pokedom </h1>
-      <main className="tile-container"> {pokemonTileJsx} </main>
+      <main className="tile-container"> {pokemonTileJsxGround} </main>
     </div>
   );
 };
